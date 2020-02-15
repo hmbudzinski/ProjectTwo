25a6006118b150adc1c7bd84879d5aabd91e6853
Hayley Budzinski <hayleybudzinski@gmail.com>
2020-02-15 14:02:00 -0700

    broke my html so hopefully this fixed it

---
+++ db/seeds.sql
@@ -0,0 +1,1 @@
+INSERT INTO users (userName, firstName, lastName, email, password, createdAt, updatedAt) VALUES ("tester", "firstTest", "lastTest", "email@email.com", "password", "2020-02-15", "2020-02-15");
