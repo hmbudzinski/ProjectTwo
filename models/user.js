327725273684b24d632900640a0d61d04c63007a
Hayley Budzinski <hayleybudzinski@gmail.com>
2020-02-18 13:56:57 -0700

    html routes working

--- models/user.js
+++ models/user.js
@@ -37,6 +37,7 @@ module.exports = function(sequelize, DataTypes) {
     User.prototype.validPassword = function(password) {
         return bcrypt.compareSync(password, this.password);
     };
+
     // Hooks are automatic methods that run during various phases of the User Model lifecycle
     // In this case, before a User is created, we will automatically hash their password
     User.addHook("beforeCreate", function(user) {
