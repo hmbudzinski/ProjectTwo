ebd9a2beeb4f95022539a1c621b785ea16d34b2a
Syd Rigel <prest261@umn.edu>
2020-02-19 21:49:20 -0700

    fixed front end, able to push to table now

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -1,6 +1,43 @@
 $(document).ready(function() {
     $('select').formSelect();
+    console.log('5');
 });
+
+// $(`input[name=${this.name}]:checked`).val()
+
+function group1() {
+    var g1 = $('input[name=group1]:checked');
+    var g1DataName = $(g1).data('name');
+    console.log('g1', g1);
+    console.log('g1DataName', g1DataName);
+    return g1DataName;
+}
+
+function group2() {
+    var g2 = $('input[name=group2]:checked');
+    var g2DataName = $(g2).data('name');
+    console.log('g2', g2);
+    console.log('g2DataName', g2DataName);
+    return g2DataName;
+}
+
+function group3() {
+    var g3 = $('input[name=group3]:checked');
+    var g3DataName = $(g3).data('name');
+    console.log('g3', g3);
+    console.log('g3DataName', g3DataName);
+    return g3DataName;
+}
+
+function fandoms() {
+    var fandom = $('#fandom').val();
+    // var fandomDataName = $(fandom).data('name');
+    console.log('fandom', fandom);
+    // console.log('fandomDataName', $('#fandom').data('name'));
+    // dont forget to Parse!! on front end
+    // stringify to avoid validation error
+    return JSON.stringify(fandom);
+}
 
 //in this file we need to add sequelize for pushing the inputted data to the existing table
 
@@ -9,6 +46,12 @@ $(document).ready(function() {
 // When user clicks add-btn
 $('#signUp').on('click', function(event) {
     event.preventDefault();
+    console.log('6');
+
+    var relationship = group1();
+    var dadJoke = group2();
+    var gif = group3();
+    var fanFunk = fandoms();
 
     // Make a newUser object
     var newUser = {
@@ -24,27 +67,21 @@ $('#signUp').on('click', function(event) {
         password: $('#password')
             .val()
             .trim(),
-        fandoms: $('#fandom')
-            .val()
-            .trim(),
-        relationship: $('#relationship')
-            .val()
-            .trim(),
-        dadJoke: $('#dadJoke')
-            .val()
-            .trim(),
+        fandoms: fanFunk,
+        relationship: relationship,
+        dadJoke: dadJoke,
         cosplay: $('#cosplay')
             .val()
             .trim(),
-        gif: $('#gif').val()
+        gif: gif
     };
 
     // Send an AJAX POST-request with jQuery
     $.post('/api/signup', newUser)
         // On success, run the following code
-        .then(function(data) {
+        .then(function(newUser) {
             // Log the data we found
-            console.log(data);
+            console.log(newUser, '2');
         });
 
     // Empty each input box by replacing the value with an empty string
