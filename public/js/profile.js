92605cba3967e7f9b9c495d02088e6e66529fc78
Mel Garza <melgarza3016@gmail.com>
2020-02-24 21:33:13 -0700

    fixed dotenv and html links

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,49 +1,50 @@
+console.log("hi");
 $(document).ready(function() {
-    // This file just does a GET request to figure out which user is logged in
-    // and updates the HTML on the page
-    $.get(`/profile/${response.dataValues.id}`).then(function(data) {
-        console.log(data);
-        $('#first-name').empty();
-        $('#first-name').text(data.firstName);
+  // This file just does a GET request to figure out which user is logged in
+  // and updates the HTML on the page
+  console.log(window.location);
+  $.get(`/profile/${response.dataValues.id}`).then(function(data) {
+    console.log(`${response.dataValues.id}`);
+    $("#first-name").empty();
+    $("#first-name").text(data.firstName);
 
-        $('#last-name').empty();
-        $('#last-name').text(data.lastName);
+    $("#last-name").empty();
+    $("#last-name").text(data.lastName);
 
-        $('#email').empty();
-        $('#email').text(data.email);
+    $("#email").empty();
+    $("#email").text(data.email);
 
-        $('#fandom').empty();
-        $('#fandom').text(data.fandoms);
+    $("#fandom").empty();
+    $("#fandom").text(data.fandoms);
 
-        $('#relationship').empty();
-        $('#relationship').text(data.relationship);
+    $("#relationship").empty();
+    $("#relationship").text(data.relationship);
 
-        $('#dad-joke').empty();
-        $('#dad-joke').text(data.dadJoke);
+    $("#dad-joke").empty();
+    $("#dad-joke").text(data.dadJoke);
 
-        $('#cosplay').empty();
-        $('#cosplay').text(data.cosplay);
+    $("#cosplay").empty();
+    $("#cosplay").text(data.cosplay);
 
-        $('#pronounce-gif').empty();
-        $('#pronounce-gif').text(data.gif);
-    });
+    $("#pronounce-gif").empty();
+    $("#pronounce-gif").text(data.gif);
+  });
 });
 
-$(document).ready(function() {
-    var logout = $('#logout');
+// $(document).ready(function() {
+//   var logout = $("#logout");
 
-    //changed html just to get it to work, can change back after its fuctioning to look better
-    logout.on('click', function(event) {
-        //console works
-        console.log('click');
+//   //changed html just to get it to work, can change back after its fuctioning to look better
+//   logout.on("click", function(event) {
+//     //console works
+//     console.log("click");
 
-        $.get('/logout')
-            .then(function() {
-                window.location.replace('/');
-            })
-            .catch(function(err) {
-                console.log(err);
-            });
-    });
-});
-
+//     $.get("/logout")
+//       .then(function() {
+//         window.location.replace("/");
+//       })
+//       .catch(function(err) {
+//         console.log(err);
+//       });
+//   });
+// });
