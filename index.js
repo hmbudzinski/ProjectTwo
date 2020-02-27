0a8772efe6045cbf19d8ee86f1f1b52a7ccccb15
Mel Garza <melgarza3016@gmail.com>
2020-02-27 13:27:24 -0700

    hidden aws key and secret

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 require("dotenv").config();
 
 console.log("my super secret variable is: ", process.env.DB_password);
+console.log("my super secret variable is: ", process.env.ACCESS_KEY);
+console.log("my super secret variable is: ", process.env.SECRET_KEY);
